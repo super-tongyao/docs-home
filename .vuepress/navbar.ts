59a018374667b19cb72f7bd21743004c5d769da3
Tongyao <58587822+super-tongyao@users.noreply.github.com>
2022-08-29 13:29:24 +0800

    Update navbar.ts

--- .vuepress/navbar.ts
+++ .vuepress/navbar.ts
@@ -1,8 +1,7 @@
-import { SidebarConfig } from "@vuepress/theme-default/lib/shared/nav";
+import { NavbarConfig } from "@vuepress/theme-default/lib/shared/nav";
 
 export default [
-    {
-        text: '初见',
-        link: '/',
-    }
-] as SidebarConfig;
+    {text: '博客笔记', link: 'https://blog.csdn.net/u014641168'},
+    {text: '镜像中心', link: 'https://mirrors.ityao.cn/'},
+    {text: 'Maven仓库', link: 'https://maven.ityao.cn/'},
+] as NavbarConfig;
