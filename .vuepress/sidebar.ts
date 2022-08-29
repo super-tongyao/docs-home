62c00a101bd6ef1c7185cbb7270e37dd12c52c6f
Tongyao <58587822+super-tongyao@users.noreply.github.com>
2022-08-29 13:29:51 +0800

    Update sidebar.ts

--- .vuepress/sidebar.ts
+++ .vuepress/sidebar.ts
@@ -2,13 +2,7 @@ import { SidebarConfig } from "@vuepress/theme-default/lib/shared/nav";
 
 export default [
     {
-        text: '我的介绍',
+        text: '初见',
         link: '/',
-    },
-    {
-        text: '我的项目',
-        collapsible: true,
-        children: ['/我的项目/简洁商城/', '/我的项目/字节小柜/'],
-    },
-    '/我的故事',
+    }
 ] as SidebarConfig;
