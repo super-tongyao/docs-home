b2303584650a23b0f8700c379d3c2d617658fbeb
Tongyao <58587822+super-tongyao@users.noreply.github.com>
2022-08-29 13:20:23 +0800

    Update navbar.ts

--- .vuepress/navbar.ts
+++ .vuepress/navbar.ts
@@ -1,6 +1,7 @@
 import { NavbarConfig } from "@vuepress/theme-default/lib/shared/nav";
 
 export default [
-    {text: '字节小柜', link: '/我的项目/字节小柜.md'},
-    {text: '简洁商城', link: '/我的项目/简洁商城.md'},
+    {text: '博客笔记', link: 'https://blog.csdn.net/u014641168'},
+    {text: '镜像中心', link: 'https://mirrors.ityao.cn/'},
+    {text: 'Maven仓库', link: 'https://maven.ityao.cn/'},
 ] as NavbarConfig;
