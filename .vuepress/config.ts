f414017630ad633b5cec73577b3d2575c57a358d
Tongyao <58587822+super-tongyao@users.noreply.github.com>
2022-08-29 13:20:56 +0800

    Update config.ts

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -18,15 +18,15 @@ export default defineUserConfig({
             !function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"JjDPBvNXNcI9wXx4",ck:"JjDPBvNXNcI9wXx4",autoTrack:true,hashMode:true});
         `],
         ['link', { rel: 'icon', href: '/favicon.ico' }],
-        ['meta', { name: 'keywords', content: '张童瑶,字节小柜'}],
+        ['meta', { name: 'keywords', content: '张童瑶,字节小柜,tongyao'}],
         ['meta', {
-            name: 'description', content:
-                '帮助你学习编程的小圈子,编程学习资源,原创编程学习经验方法,程序员求职校招指南,原创编程项目'
+            name: 'description', 
+			content: '编程学习资源,原创编程学习经验方法,程序员求职校招指南,原创编程项目'
         }],
     ],
     theme: defaultTheme({
         logo: 'logo.png',
-        repo: 'super-tongyao/docs-home',
+        repo: 'super-tongyao',
         sidebar,
         navbar,
         editLink: false,
@@ -43,11 +43,11 @@ export default defineUserConfig({
             staticIcon: true,
         }),
         seoPlugin({
-            hostname: 'https://baidu.com',
+            hostname: 'https://ityao.cn',
             author: 'Tongyao',
         }),
         sitemapPlugin({
-            hostname: 'https://baidu.com',
+            hostname: 'https://ityao.cn',
         }),
         searchConsolePlugin({
             // 改成自己的百度统计 id：https://tongji.baidu.com/
