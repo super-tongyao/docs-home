4550d96465e33e501d57007571d9af268aa42af1
tongyao <1973689961@qq.com>
2023-05-12 10:53:54 +0800

    submit

---
+++ assets/Wall.html.2741da18.js
@@ -0,0 +1,91 @@
+import{_ as t,o as a,c as d,a as e,b as l,e as n,d as s,r}from"./app.783a4578.js";const c={},o=e("h1",{id:"wall",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#wall","aria-hidden":"true"},"#"),n(" Wall")],-1),v=e("p",null,[e("img",{src:"https://img.shields.io/badge/Vue-3.2.13-brightgreen.svg",alt:"Vue"}),e("img",{src:"https://img.shields.io/badge/Spring Boot-2.2.6-brightgreen.svg",alt:"Spring Boot 2.2.6"})],-1),u=e("p",null,"Wall\u662F\u4E00\u6B3E\u5FEB\u901F\u5206\u4EAB\u8D44\u6E90\u5E94\u7528\u7A0B\u5E8F\u3002\u4FD7\u79F0\u201C\u7167\u7247\u5899\u3001\u89C6\u9891\u5899\u201D\uFF0C\u57FA\u4E8EVue3 + Spring Boot\u5F00\u53D1\u7684\u4E91\u5171\u4EAB\u8D44\u6E90\u5E94\u7528\u7CFB\u7EDF\u3002\u5FEB\u901F\u5206\u4EAB\u53D1\u5E03\u7167\u7247\u548C\u89C6\u9891\u5E73\u53F0\u3002\u517C\u5BB9PC\u7AEF\u548C\u79FB\u52A8\u7AEF\uFF0C\u652F\u6301\u7AEF\u5BF9\u7AEF\u8DE8\u5E73\u53F0\u4E0A\u4F20\u8D44\u6E90\u6587\u4EF6\u3002",-1),p=n("\u6F14\u793A\u5730\u5740\uFF1A"),m={href:"https://demo-wall.ityao.cn",target:"_blank",rel:"noopener noreferrer"},h=n("https://demo-wall.ityao.cn"),b=n("\u540E\u53F0\u7BA1\u7406\uFF1A"),g={href:"https://demo-wall.ityao.cn/login",target:"_blank",rel:"noopener noreferrer"},_=n("https://demo-wall.ityao.cn/login"),x=n("\uFF0C\u8D26\u53F7\u5BC6\u7801\uFF1Aadmin/123456"),f=s('<p>\u{1F31F}\u5982\u679C\u8FD9\u4E2A\u9879\u76EE\u8BA9\u4F60\u6709\u6240\u6536\u83B7\uFF0C\u8BB0\u5F97 Star \u5173\u6CE8\u54E6\uFF0C\u8FD9\u5BF9\u6211\u662F\u975E\u5E38\u4E0D\u9519\u7684\u9F13\u52B1\u4E0E\u652F\u6301\u3002</p><h2 id="\u6F14\u793A\u622A\u56FE" tabindex="-1"><a class="header-anchor" href="#\u6F14\u793A\u622A\u56FE" aria-hidden="true">#</a> \u6F14\u793A\u622A\u56FE</h2><p><img src="https://img-blog.csdnimg.cn/d90a3605852049e59e6129b6ea356d21.png" alt=""></p><p><img src="https://img-blog.csdnimg.cn/b09c6dd1af074f77ba17575a32a49f6e.png" alt=""></p><p><img src="https://img-blog.csdnimg.cn/42a33e6001104fda84fd2a66955cad90.png" alt=""></p><p><img src="https://img-blog.csdnimg.cn/f170a54d25f54aeaacf75aa62177f207.png" alt=""></p><p><img src="https://img-blog.csdnimg.cn/da2f0ec64c97424cae4e4c57e71d4488.png" alt=""></p><p><img src="https://img-blog.csdnimg.cn/f8e02fe368c04000b82f875af872681b.jpeg" alt=""></p><h2 id="\u5F00\u53D1\u8005\u540D\u5355" tabindex="-1"><a class="header-anchor" href="#\u5F00\u53D1\u8005\u540D\u5355" aria-hidden="true">#</a> \u5F00\u53D1\u8005\u540D\u5355</h2><p>Wall\u8FD8\u6709\u5F88\u591A\u4E0D\u8DB3\u4E4B\u5904\uFF0C\u6BD4\u5982\u90E8\u5206\u79FB\u52A8\u7AEF\u673A\u578B\u517C\u5BB9\u7B49\u76F8\u5173\u95EE\u9898\u3002</p><p>\u6216\u8BB8\u4F60\u53EF\u4EE5\u52A0\u5165Wall\u56E2\u961F\uFF0C\u6211\u4EEC\u4E00\u8D77\u8D21\u732E\u4EE3\u7801\u3002<a href="#%E5%85%B1%E5%90%8C%E5%8D%8F%E4%BD%9C">\u7533\u8BF7\u52A0\u5165</a></p><p>\u4E0B\u9762\u8868\u683C\u4E2D\u51FA\u73B0\u4F60\u7684\u5934\u50CF\u53CAGitHub\u8D26\u53F7\u5730\u5740\uFF0C\u89C6\u4E3AWall\u56E2\u961F\u6210\u5458\u3002</p>',12),w=e("thead",null,[e("tr",null,[e("th",null,"\u540D\u79F0"),e("th",null,"Github")])],-1),y=e("td",null,"Tongyao",-1),W={href:"https://github.com/super-tongyao",target:"_blank",rel:"noopener noreferrer"},S=n("https://github.com/super-tongyao"),k=e("td",null,"Chen Shunhong",-1),B={href:"https://github.com/ChenShunhong",target:"_blank",rel:"noopener noreferrer"},L=n("https://github.com/ChenShunhong"),$=e("h2",{id:"\u5B89\u88C5\u6559\u7A0B",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#\u5B89\u88C5\u6559\u7A0B","aria-hidden":"true"},"#"),n(" \u5B89\u88C5\u6559\u7A0B")],-1),N=n("1\u3001\u4E0B\u8F7D\u5730\u5740\uFF1A"),j={href:"https://github.com/super-tongyao/wall/releases",target:"_blank",rel:"noopener noreferrer"},C=n("https://github.com/super-tongyao/wall/releases"),E=n("\uFF0C\u4EE5\u6700\u65B0\u7248\u4E3A\u51C6\u3002"),V=s(`<p>2\u3001\u89E3\u538B\u7F29\u5305\uFF0C\u91CC\u9762\u6709\u4E24\u4E2A\u6587\u4EF6\u5939\uFF0C\u4E24\u4E2A\u6267\u884C\u811A\u672C\uFF0C\u4E00\u4E2ASQL\u811A\u672C\u3002</p><p><img src="https://img-blog.csdnimg.cn/d41c213041d543ddacdc3b71f7a3a3ec.png" alt=""></p><ul><li>wall\uFF1A\u7F16\u8BD1\u597D\u7684\u524D\u7AEF\u9875\u9762\u3002</li><li>wall-service\uFF1A\u7F16\u8BD1\u597D\u7684\u540E\u7AEFjar\u7A0B\u5E8F\u5305\u3002</li><li>wall.sql\uFF1A\u540E\u7AEF\u6570\u636E\u5E93\u3002</li><li>startup.bat\uFF1AWall\u542F\u52A8\u811A\u672C\u3002</li><li>stop.bat\uFF1AWall\u505C\u6B62\u811A\u672C\u3002</li></ul><p>3\u3001Wall\u73AF\u5883\u7248\u672C\u53EA\u9700\u6CE8\u91CD\u5173\u6CE8\u6570\u636E\u5E93\u7248\u672C\u5373\u53EF\u3002</p><ul><li>MySQL 5.7 \u4EE5\u4E0A\u7248\u672C</li><li>Jdk 1.8 \u4EE5\u4E0A\u7248\u672C\uFF08Wall\u81EA\u5E26\uFF09</li><li>Nginx 1.16 \u4EE5\u4E0A\u7248\u672C\uFF08Wall\u81EA\u5E26\uFF09</li></ul><p>4\u3001\u5982\u679C\u4F60\u662F\u5F00\u53D1\u8005\uFF08\u7A0B\u5E8F\u5458\uFF09\uFF0C\u5982\u53EA\u9700\u8981\u524D\u7AEF\u7F16\u8BD1\u7A0B\u5E8F\u9875\u9762\u548C\u540E\u7AEF\u670D\u52A1jar\u5305\uFF0C\u90A3\u4E48\u53EA\u4ECE\u5982\u4E0B\u8DEF\u5F84\u4E2D\u62BD\u53D6\u6587\u4EF6\uFF0C\u5230\u4F60\u7684\u670D\u52A1\u5668\u624B\u52A8\u547D\u4EE4\u542F\u52A8Wall\u670D\u52A1\u3002</p><ul><li><p>Wall\u524D\u7AEF\u9875\u9762\u7F16\u8BD1\u6587\u4EF6\uFF1Awall/html/wall</p></li><li><p>Wall\u540E\u7AEF\u670D\u52A1Jar\u5305\uFF1Awall-service/jar/wall.jar</p></li></ul><h3 id="\u7A0B\u5E8F\u5B89\u88C5" tabindex="-1"><a class="header-anchor" href="#\u7A0B\u5E8F\u5B89\u88C5" aria-hidden="true">#</a> \u7A0B\u5E8F\u5B89\u88C5</h3><p>\u81EA2.0.0\u7248\u672C\u4EE5\u4E0A\u8D77\uFF0C\u6240\u6709Wall\u7A0B\u5E8F\u91C7\u7528\u4E00\u952E\u542F\u52A8\uFF0C\u5185\u7F6ENginx\u3001JDK\uFF0C\u65E0\u9700\u518D\u914D\u7F6ENginx\u7B49\u76F8\u5173\u6587\u4EF6\u64CD\u4F5C\u3002</p><p>1\u3001\u5BFC\u5165<code>Wall.sql</code>\u6587\u4EF6\u5230MySQL\u6570\u636E\u5E93\u3002</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>mysql&gt; source wall.sql
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><p>2\u3001\u4FEE\u6539wall-service/config/application.yml\u914D\u7F6E\u6587\u4EF6\uFF0C\u5E76\u4FEE\u6539\u4F60\u672C\u5730\u7684MySQL\u6570\u636E\u5E93\u8FDE\u63A5\u7AEF\u53E3\u53CA\u8D26\u53F7\u5BC6\u7801\u3002</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code># project prot
+server:
+  port: 9999
+
+# database config
+mysql:
+  database: wall
+  port: 3306
+  ip: 127.0.0.1
+  username: root
+  password: root
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>3\u3001\u540E\u7AEF\u670D\u52A1\u652F\u6301\u4E24\u79CD\u73AF\u5883\u4E0B\u5FEB\u6377\u542F\u52A8\u3002</p><ul><li><p>Windows\uFF1A\u53CC\u51FB<code>startup.bat</code>\u6587\u4EF6\u542F\u52A8\u3002</p></li><li><p>Linux\uFF1A\u6267\u884C\u6839\u76EE\u5F55<code>./startup.sh</code>\u6587\u4EF6\u542F\u52A8\u3002</p></li></ul><p>\u5728Linux\u4E0B\uFF0C\u5982\u679C\u4F60\u60F3\u5173\u95ED\u4F1A\u8BDD\u540E\u7EE7\u7EED\u8FD0\u884CWall\uFF0C\u90A3\u4E48\u53EF\u4EE5\u642D\u914Dnohup\u4F7F\u7528\u3002</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>nohup ./startup.sh r&gt; /dev/null 2&gt; /dev/null &amp;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><p>4\u3001\u6D4F\u89C8\u5668\u8BBF\u95EEWall\u7A0B\u5E8F\u3002</p><p>\u6D4F\u89C8\u5668\u8F93\u5165\uFF1A<code>http://localhost:80</code>\uFF0C\u6B63\u5E38\u663E\u793A\u9875\u9762\u53CA\u64CD\u4F5C\u6570\u636E\uFF0C\u81F3\u6B64\u5B8C\u6210\u5B89\u88C5\u3002</p><p>\u540E\u53F0\u7BA1\u7406\uFF1A<code>http://localhost:80/login</code>\uFF0C\u9ED8\u8BA4\u8D26\u53F7\u5BC6\u7801\uFF1Aadmin/123456\uFF0C\u5982\u6709\u95EE\u9898\uFF0C\u8BF7\u63D0\u4EA4Issues\u3002</p><h3 id="\u62D3\u5C55\u914D\u7F6E-\u53EF\u9009" tabindex="-1"><a class="header-anchor" href="#\u62D3\u5C55\u914D\u7F6E-\u53EF\u9009" aria-hidden="true">#</a> \u62D3\u5C55\u914D\u7F6E\uFF08\u53EF\u9009\uFF09</h3>`,21),q=n("\u4E3B\u8981\u7B80\u5316Wall\u5B89\u88C5\u7A0B\u5E8F\u540E\u7684\u4E00\u4E9B\u57FA\u672C\u914D\u7F6E\uFF0C\u5982\u6682\u4E0D\u9700\u8981\uFF0C\u65E0\u9700\u914D\u7F6E\uFF0C\u5176\u4ED6\u66F4\u591A\u914D\u7F6E\uFF0C\u8BF7\u53C2\u8003"),G={href:"http://nginx.org/en/docs/",target:"_blank",rel:"noopener noreferrer"},M=n("Nginx\u5B98\u7F51"),I=n("\u6587\u6863\u3002"),z=s(`<p>1\u3001\u524D\u7AEF\u9875\u9762\u7AEF\u53E3\u4FEE\u6539</p><p>\u9ED8\u8BA4\u7AEF\u53E3\u4E3A80\uFF0C\u4FEE\u6539wall/config/nginx.conf\u914D\u7F6E\u6587\u4EF6\u3002\u5E76\u4FEE\u6539\u4F60\u7684\u524D\u7AEF\u7AEF\u53E3\uFF0C\u672C\u6587\u8BBE\u7F6E\u4E3A8888\u3002</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>server {
+	listen       8888;
+	server_name  localhost;
+	
+	underscores_in_headers on;
+
+	location / {
+		# \u6620\u5C04\u5BF9\u5E94nginx/html\u4E0B\u7684wall\u524D\u7AEF\u7A0B\u5E8F
+		root html/wall;
+		try_files $uri $uri/ /index.html;
+	}
+	.......
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>2\u3001\u4FEE\u6539\u540E\u7AEF\u670D\u52A1\u7AEF\u53E3</p><p>\u9ED8\u8BA4\u7AEF\u53E3\u4E3A9999\uFF0C\u4FEE\u6539wall-service/config/application.yml\u914D\u7F6E\u6587\u4EF6\u3002\u5E76\u4FEE\u6539\u4F60\u7684\u540E\u7AEF\u7AEF\u53E3\uFF0C\u672C\u6587\u8BBE\u7F6E\u4E3A8080\u3002</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code># project prot
+server:
+  port: 8080
+  
+......
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>\u6CE8\u610F\uFF1A\u5982\u679C\u4F60\u4FEE\u6539\u4E86\u540E\u7AEF\u670D\u52A1\u7AEF\u53E3\uFF0C\u90A3\u4E48\u524D\u7AEF\u6620\u5C04\u914D\u7F6E\u4E2D\uFF0C\u4E5F\u9700\u8981\u4FEE\u6539\u6620\u5C04\u540E\u7AEF\u670D\u52A1\u7AEF\u53E3\uFF0C\u5426\u5219\u65E0\u6CD5\u627E\u5230\u540E\u7AEF\u63A5\u53E3\u7B49\u95EE\u9898\u3002</p><p>\u4FEE\u6539wall/config/nginx.conf\u914D\u7F6E\u6587\u4EF6\u3002\u627E\u5230\u6620\u5C04\u540E\u7AEF\u670D\u52A1\u7684/api\uFF0C\u6539\u4E3A\u5BF9\u5E948080\u7AEF\u53E3\u540E\uFF0C\u91CD\u542FWall\u5373\u53EF\u3002</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>server {
+	listen       80;
+	server_name  localhost;
+	......
+	
+	# \u540E\u7AEF\u670D\u52A1\u5730\u5740
+	location /api{
+		rewrite  ^/api/(.*)$ /$1 break;
+		proxy_set_header Host $host;
+		proxy_set_header X-Real-IP $remote_addr;
+		proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+		
+		# \u8F6C\u53D1Wall\u7684\u540E\u53F0\u670D\u52A1\u5730\u5740
+		proxy_pass   http://localhost:8080;
+	}
+	......
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>3\u3001\u7ED1\u5B9A\u57DF\u540D</p>`,10),P=n("\u9996\u5148\u4E91\u670D\u52A1\u5546\u89E3\u6790\u57DF\u540D\u5230\u672C\u670D\u52A1\u5668IP\uFF0C\u4FEE\u6539wall/config/nginx.conf\u914D\u7F6E\u6587\u4EF6\uFF0C\u6539\u6210\u4F60\u7684\u57DF\u540D\u3002\u66F4\u591A\u5185\u5BB9\u8BF7\u67E5\u770B"),Q={href:"http://nginx.org/en/docs/",target:"_blank",rel:"noopener noreferrer"},D=n("Nginx\u5B98\u7F51"),F=n("\u3002"),H=s(`<div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>server {
+	listen       80;
+	server_name  \u4F60\u7684\u57DF\u540D\u5730\u5740;
+	
+	underscores_in_headers on;
+
+	location / {
+		# \u6620\u5C04\u5BF9\u5E94nginx/html\u4E0B\u7684wall\u524D\u7AEF\u7A0B\u5E8F
+		root html/wall;
+		try_files $uri $uri/ /index.html;
+	}
+	.......
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>4\u3001\u4F7F\u7528https</p>`,2),J=n("\u786E\u4FDD\u4F60\u6709\u53EF\u7528\u7684SSL\u8BC1\u4E66\uFF0C\u4FEE\u6539wall/config/nginx.conf\u914D\u7F6E\u6587\u4EF6\uFF0C\u589E\u52A0\u5982\u4E0B\u914D\u7F6E\u3002\u66F4\u591A\u5185\u5BB9\u8BF7\u67E5\u770B"),T={href:"http://nginx.org/en/docs/http/configuring_https_servers.html",target:"_blank",rel:"noopener noreferrer"},X=n("SSL\u914D\u7F6E"),K=n("\u3002"),R=s(`<div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>server {
+	listen       80;
+	server_name  \u4F60\u7684\u57DF\u540D\u5730\u5740;
+	
+	ssl_certificate \u4F60\u7684SSL\u57DF\u540Dpem\u8BC1\u4E66.pem;
+	ssl_certificate_key \u4F60\u7684SSL\u57DF\u540Dkey\u8BC1\u4E66.key;
+	
+	underscores_in_headers on;
+
+	location / {
+		# \u6620\u5C04\u5BF9\u5E94nginx/html\u4E0B\u7684wall\u524D\u7AEF\u7A0B\u5E8F
+		root html/wall;
+		try_files $uri $uri/ /index.html;
+	}
+	.......
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>5\u3001\u4FEE\u6539Wall\u4E0A\u4F20\u6587\u4EF6\u5927\u5C0F\u9650\u5236</p><p>Wall\u9ED8\u8BA4\u4E0A\u4F20\u5927\u5C0F\u4E3A1024MB\uFF081GB\uFF09\uFF0C\u4FEE\u6539wall/config/nginx.conf\u914D\u7F6E\u6587\u4EF6\uFF0C\u6682\u6539\u4E3A2048\uFF082GB\uFF09\u3002</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>http {
+	......
+	
+	client_max_body_size 2048m;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>\u4FEE\u6539wall-service/config/application.yml\u914D\u7F6E\u6587\u4EF6\uFF0C\u6682\u6539\u4E3A2048\uFF082GB\uFF09\u3002</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code># spring config
+spring:
+  ......
+
+# file upload size
+  servlet:
+    multipart:
+      max-file-size: 2048MB
+      max-request-size: 2048MB
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="\u5171\u540C\u534F\u4F5C" tabindex="-1"><a class="header-anchor" href="#\u5171\u540C\u534F\u4F5C" aria-hidden="true">#</a> \u5171\u540C\u534F\u4F5C</h2><p>\u5728\u7EBF\u63D0\u4EA4Push\u4F60\u7684\u4EE3\u7801\u3002</p><h2 id="github" tabindex="-1"><a class="header-anchor" href="#github" aria-hidden="true">#</a> GitHub</h2>`,9),A={href:"https://github.com/super-tongyao/wall",target:"_blank",rel:"noopener noreferrer"},O=n("https://github.com/super-tongyao/wall");function U(Y,Z){const i=r("ExternalLinkIcon");return a(),d("div",null,[o,v,u,e("ul",null,[e("li",null,[e("p",null,[p,e("a",m,[h,l(i)])])]),e("li",null,[e("p",null,[b,e("a",g,[_,l(i)]),x])])]),f,e("table",null,[w,e("tbody",null,[e("tr",null,[y,e("td",null,[e("a",W,[S,l(i)])])]),e("tr",null,[k,e("td",null,[e("a",B,[L,l(i)])])])])]),$,e("p",null,[N,e("a",j,[C,l(i)]),E]),V,e("p",null,[q,e("a",G,[M,l(i)]),I]),z,e("p",null,[P,e("a",Q,[D,l(i)]),F]),H,e("p",null,[J,e("a",T,[X,l(i)]),K]),R,e("p",null,[e("a",A,[O,l(i)])])])}var ne=t(c,[["render",U],["__file","Wall.html.vue"]]);export{ne as default};
